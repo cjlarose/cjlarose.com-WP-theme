4666e029fdf9dfebd7c1b4df080cb2259fc2cdf8
Chris LaRose <cjlarose@gmail.com>
2011-08-16 14:20:32 -0700

    Changed image sizes for project proviews in sidebar-project for newly formatted sidebar

--- sidebar-project.php
+++ sidebar-project.php
@@ -4,16 +4,16 @@
         <?php
                 $post_query = new WP_Query(array(
                 'post_type' => 'project',
-                'posts_per_page' => 10
+                'posts_per_page' => 3
         ));
         ?>
-        <ul class="project-thumb-list">
-        <?php $counter = 0; ?>
-        <?php if ( $post_query->have_posts() ) while ( $post_query->have_posts() ) : $counter++; $post_query->the_post(); ?>
-            <li class="span-4<?php echo ($counter % 2 == 0)?" last":""; ?>">
+        <ul class="sidebar-project-list">
+
+        <?php if ( $post_query->have_posts() ) while ( $post_query->have_posts() ) : $post_query->the_post(); ?>
+            <li class="span-6 last">
             
             <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail( 'project-thumbnail', array('title' => get_the_title()) ); ?>
+                <?php the_post_thumbnail( 'project-medium', array('title' => get_the_title()) ); ?>
             </a>
             <?php //the_excerpt(); ?>
             
