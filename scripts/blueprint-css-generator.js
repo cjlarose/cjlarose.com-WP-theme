4ea19f3671f18dfc5f9d574ce098d197412ca3b8
Chris LaRose <cjlarose@gmail.com>
2011-08-15 17:03:42 -0700

    Modified bp jQuery javascript so that values can be changed with up/down arrow keys

--- scripts/blueprint-css-generator.js
+++ scripts/blueprint-css-generator.js
@@ -14,4 +14,18 @@ $j(function() {
     $j('#bp-gen-form input[type=submit]').parent().before('<p id="container-width">Container width: <strong></strong>px</p>');
     update_container_width();
     $j('#bp-gen-form input[type=text]').keyup(update_container_width);
+    
+    $j('#bp-gen-form input[type=text]').keydown(function(event) {
+        
+        if (event.keyCode == '38' || event.keyCode == '40') {
+            event.preventDefault();
+            if (event.keyCode == '38') {
+                $j(this).val($j(this).val()*1+1);
+            } else if (event.keyCode == '40') {
+                $j(this).val($j(this).val()*1-1);
+            }
+        }
+        
+    });
+    
 });
